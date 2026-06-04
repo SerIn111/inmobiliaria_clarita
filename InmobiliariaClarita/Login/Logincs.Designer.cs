32c2c43998899d3fcaac2444e5844abf198b7633
Sergio <amayasergio31@gmail.com>
2026-06-04 11:29:58 -0600

    Segundo Commit

--- InmobiliariaClarita/Login/Logincs.Designer.cs
+++ InmobiliariaClarita/Login/Logincs.Designer.cs
@@ -28,19 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Logincs));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            txtUsuario = new TextBox();
+            txtContra = new TextBox();
+            btnAceptar = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(292, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(253, 39);
+            label1.TabIndex = 0;
+            label1.Text = "Inicio de sesion";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(51, 112);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(194, 214);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(292, 112);
+            label2.Name = "label2";
+            label2.Size = new Size(96, 29);
+            label2.TabIndex = 2;
+            label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(301, 221);
+            label3.Name = "label3";
+            label3.Size = new Size(136, 29);
+            label3.TabIndex = 3;
+            label3.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtUsuario.Location = new Point(301, 144);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(244, 30);
+            txtUsuario.TabIndex = 1;
+            // 
+            // txtContra
+            // 
+            txtContra.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtContra.Location = new Point(301, 253);
+            txtContra.Name = "txtContra";
+            txtContra.Size = new Size(244, 30);
+            txtContra.TabIndex = 2;
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.BackColor = Color.White;
+            btnAceptar.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAceptar.Location = new Point(316, 316);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(218, 58);
+            btnAceptar.TabIndex = 3;
+            btnAceptar.Text = "Aceptar";
+            btnAceptar.UseVisualStyleBackColor = false;
             // 
             // Logincs
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PapayaWhip;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnAceptar);
+            Controls.Add(txtContra);
+            Controls.Add(txtUsuario);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Logincs";
-            Text = "Logincs";
+            Text = "Login";
             Load += Logincs_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtUsuario;
+        private TextBox txtContra;
+        private Button btnAceptar;
     }
 }
--- InmobiliariaClarita/Program.cs
+++ InmobiliariaClarita/Program.cs
@@ -1,3 +1,5 @@
+using InmobiliariaClarita.Login;
+
 namespace InmobiliariaClarita
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace InmobiliariaClarita
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Logincs());
         }
     }
 }
