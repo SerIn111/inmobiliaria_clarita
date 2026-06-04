ed05a69953b2ef41ee98d3c323e9389786072854
Sergio <amayasergio31@gmail.com>
2026-06-04 09:37:49 -0600

    Primer commit

---
+++ InmobiliariaClarita/Login/Logincs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InmobiliariaClarita.Login
+{
+    public partial class Logincs : Form
+    {
+        public Logincs()
+        {
+            InitializeComponent();
+        }
+
+        private void Logincs_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
