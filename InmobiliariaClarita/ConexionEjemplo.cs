51fd278b06f7977892f257dd4ef3969cead9f645
Sergio <amayasergio31@gmail.com>
2026-06-09 10:51:33 -0600

    Vuelvo a hacer el push porque hay cambios que no veo en el GitHub, este es por si acaso

--- InmobiliariaClarita/ConexionEjemplo.cs
+++ InmobiliariaClarita/ConexionEjemplo.cs
@@ -5,8 +5,8 @@ public static class ConexionEjemplo
     //Este es el archivo plantilla, este es el que se sube al repositorio
     private static string cadenaConexion = 
         "Server=PONER_IP_AWS,1433;" +
-        "Database=NombreDeLaDB;" +
-        "User Id=Usuario;" +
+        "Database=NOMBRE_DE_LA_DB;" +
+        "User Id=USUARIO;" +
         "Password=PONER_CONTRASEÑA_AQUÍ;" +
         "TrustServerCertificate=True;";
 
